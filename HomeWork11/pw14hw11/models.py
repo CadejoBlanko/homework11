7feda3018b353454804e180f8da7035deb87283b
Oleksandr Martyniuk <sasha.martynuk.1@gmail.com>
2023-10-15 14:47:55 +0300

    Update models.py

--- HomeWork11/pw14hw11/models.py
+++ HomeWork11/pw14hw11/models.py
@@ -1,7 +1,12 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, Boolean, Table, UniqueConstraint, func
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
 
 class Contact(Base):
     __tablename__ = 'contacts'
@@ -12,3 +17,11 @@ class Contact(Base):
     phone_number = Column(String)
     email = Column(String, index=True)
     birthdate = Column(Date)
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(150), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    refresh_token = Column(String(255), nullable=True)
