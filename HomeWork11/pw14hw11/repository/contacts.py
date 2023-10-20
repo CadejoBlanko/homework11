bc2536f843f0b44db7c64ed22e1e37e9f13b5af7
Oleksandr Martyniuk <sasha.martynuk.1@gmail.com>
2023-10-20 20:19:13 +0300

    Update contacts.py

--- HomeWork11/pw14hw11/repository/contacts.py
+++ HomeWork11/pw14hw11/repository/contacts.py
@@ -5,11 +5,13 @@ from sqlalchemy import and_
 from models import Contact, User
 from schemas import Contact as ContactModel
 
+
 async def get_contacts(skip: int, limit: int, user: User, db: Session) -> List[Contact]:
     return db.query(Contact).filter(Contact.user_id == user.id).offset(skip).limit(limit).all()
 
 async def get_contact(contact_id: int, user: User, db: Session) -> Contact:
     return db.query(Contact).filter(and_(Contact.id == contact_id, Contact.user_id == user.id)).first()
+
 
 async def create_contact(body: ContactModel, user: User, db: Session) -> Contact:
     contact = Contact(
@@ -25,6 +27,7 @@ async def create_contact(body: ContactModel, user: User, db: Session) -> Contact
     db.refresh(contact)
     return contact
 
+
 async def update_contact(contact_id: int, body: ContactModel, user: User, db: Session) -> Contact | None:
     contact = db.query(Contact).filter(and_(Contact.id == contact_id, Contact.user_id == user.id)).first()
     if contact:
@@ -36,10 +39,10 @@ async def update_contact(contact_id: int, body: ContactModel, user: User, db: Se
         db.commit()
     return contact
 
+
 async def remove_contact(contact_id: int, user: User, db: Session) -> Contact | None:
     contact = db.query(Contact).filter(and_(Contact.id == contact_id, Contact.user_id == user.id)).first()
     if contact:
         db.delete(contact)
         db.commit()
     return contact
-
