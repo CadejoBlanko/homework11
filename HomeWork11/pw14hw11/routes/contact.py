ebdc49770a9c17c1cb9b98c6b23d4e17adb9e546
Oleksandr Martyniuk <sasha.martynuk.1@gmail.com>
2023-10-20 20:19:21 +0300

    Update contact.py

--- HomeWork11/pw14hw11/routes/contact.py
+++ HomeWork11/pw14hw11/routes/contact.py
@@ -8,13 +8,16 @@ from schemas import Contact as ContactModel
 from routes.auth import auth_service
 from repository import contacts as repository_contacts
 
+
 router = APIRouter(prefix='/contacts', tags=['contacts'])
+
 
 @router.get("/")
 async def read_contacts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db),
                         current_user: User = Depends(auth_service.get_current_user)):
     contacts = await repository_contacts.get_contacts(skip, limit, current_user, db)
     return contacts
+
 
 @router.get("/{contact_id}")
 async def read_contact(contact_id: int, db: Session = Depends(get_db),
@@ -24,10 +27,12 @@ async def read_contact(contact_id: int, db: Session = Depends(get_db),
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found")
     return contact
 
+
 @router.post("/")
 async def create_contact(body: ContactModel, db: Session = Depends(get_db),
                          current_user: User = Depends(auth_service.get_current_user)):
     return await repository_contacts.create_contact(body, current_user, db)
+
 
 @router.put("/{contact_id}")
 async def update_contact(contact_id: int, body: ContactModel, db: Session = Depends(get_db),
@@ -36,6 +41,7 @@ async def update_contact(contact_id: int, body: ContactModel, db: Session = Depe
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found")
     return contact
+
 
 @router.delete("/{contact_id}")
 async def remove_contact(contact_id: int, db: Session = Depends(get_db),
