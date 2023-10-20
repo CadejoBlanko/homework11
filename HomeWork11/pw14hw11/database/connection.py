1bc45ae86488e94808265a231275a58cd7ea3dfb
Oleksandr Martyniuk <sasha.martynuk.1@gmail.com>
2023-10-20 20:19:07 +0300

    Update connection.py

--- HomeWork11/pw14hw11/database/connection.py
+++ HomeWork11/pw14hw11/database/connection.py
@@ -4,15 +4,16 @@ from sqlalchemy.orm import sessionmaker
 
 from models import Base
 
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 DATABASE_URL = "postgresql+psycopg2://postgres:567234@127.0.0.1:5432/postgres"
 engine = create_engine(DATABASE_URL)
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
+
 
 def get_db():
     db = SessionLocal()
