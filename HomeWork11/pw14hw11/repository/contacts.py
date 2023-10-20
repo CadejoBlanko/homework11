ae0667253b3c6cf6abca905a1dbe1626708883fb
Oleksandr Martyniuk <sasha.martynuk.1@gmail.com>
2023-10-20 20:16:01 +0300

    Create contacts.py

---
+++ HomeWork11/pw14hw11/repository/contacts.py
@@ -0,0 +1,45 @@
+from typing import List
+from sqlalchemy.orm import Session
+from sqlalchemy import and_
+
+from models import Contact, User
+from schemas import Contact as ContactModel
+
+async def get_contacts(skip: int, limit: int, user: User, db: Session) -> List[Contact]:
+    return db.query(Contact).filter(Contact.user_id == user.id).offset(skip).limit(limit).all()
+
+async def get_contact(contact_id: int, user: User, db: Session) -> Contact:
+    return db.query(Contact).filter(and_(Contact.id == contact_id, Contact.user_id == user.id)).first()
+
+async def create_contact(body: ContactModel, user: User, db: Session) -> Contact:
+    contact = Contact(
+        first_name=body.first_name,
+        last_name=body.last_name,
+        phone_number=body.phone_number,
+        email=body.email,
+        birthdate=body.birthdate,
+        user_id=user.id
+    )
+    db.add(contact)
+    db.commit()
+    db.refresh(contact)
+    return contact
+
+async def update_contact(contact_id: int, body: ContactModel, user: User, db: Session) -> Contact | None:
+    contact = db.query(Contact).filter(and_(Contact.id == contact_id, Contact.user_id == user.id)).first()
+    if contact:
+        contact.first_name = body.first_name
+        contact.last_name = body.last_name
+        contact.phone_number = body.phone_number
+        contact.email = body.email
+        contact.birthdate = body.birthdate
+        db.commit()
+    return contact
+
+async def remove_contact(contact_id: int, user: User, db: Session) -> Contact | None:
+    contact = db.query(Contact).filter(and_(Contact.id == contact_id, Contact.user_id == user.id)).first()
+    if contact:
+        db.delete(contact)
+        db.commit()
+    return contact
+
