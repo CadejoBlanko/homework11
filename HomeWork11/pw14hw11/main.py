399fb1a51b2686697fd6de7cc019a7608e8e69f4
Oleksandr Martyniuk <sasha.martynuk.1@gmail.com>
2023-10-29 19:14:30 +0200

    Update main.py

--- HomeWork11/pw14hw11/main.py
+++ HomeWork11/pw14hw11/main.py
@@ -1,3 +1,14 @@
+"""
+FastAPI Application
+===================
+
+This is a FastAPI application for managing contacts.
+
+It includes endpoints for sign up, login, creating, retrieving,
+updating, and deleting contacts.
+
+"""
+
 from datetime import date, timedelta
 
 from fastapi import FastAPI, HTTPException, status, Depends, Query, Security
@@ -14,12 +25,28 @@ from routes import auth, contact
 app = FastAPI()
 security = HTTPBearer()
 
+# Include routers
 app.include_router(auth.router, prefix='/api')
 app.include_router(contact.router, prefix='/api')
 
 
 @app.post("/signup")
 async def signup(body: UserModel, db: Session = Depends(get_db)):
+    """
+    Sign Up Endpoint
+
+    Register a new user.
+
+    Args:
+        body (UserModel): User registration information.
+
+    Returns:
+        dict: A dictionary containing the email of the newly registered user.
+
+    Raises:
+        HTTPException: If the account already exists.
+
+    """
     exist_user = db.query(User).filter(User.email == body.username).first()
     if exist_user:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account already exists")
@@ -32,6 +59,21 @@ async def signup(body: UserModel, db: Session = Depends(get_db)):
 
 @app.post("/login")
 async def login(body: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    """
+    Login Endpoint
+
+    Log in an existing user.
+
+    Args:
+        body (OAuth2PasswordRequestForm): User login information.
+
+    Returns:
+        dict: Access and refresh tokens along with the token type.
+
+    Raises:
+        HTTPException: If the email or password is invalid.
+
+    """
     user = db.query(User).filter(User.email == body.username).first()
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email")
@@ -46,6 +88,21 @@ async def login(body: OAuth2PasswordRequestForm = Depends(), db: Session = Depen
 
 @app.get('/refresh_token')
 async def refresh_token(credentials: HTTPAuthorizationCredentials = Security(security), db: Session = Depends(get_db)):
+    """
+    Refresh Token Endpoint
+
+    Refresh an expired access token.
+
+    Args:
+        credentials (HTTPAuthorizationCredentials): User credentials with refresh token.
+
+    Returns:
+        dict: Access and refresh tokens along with the token type.
+
+    Raises:
+        HTTPException: If the refresh token is invalid.
+
+    """
     token = credentials.credentials
     email = await auth_service.create_refresh_token(token)
     user = db.query(User).filter(User.email == email).first()
@@ -63,16 +120,49 @@ async def refresh_token(credentials: HTTPAuthorizationCredentials = Security(sec
 
 @app.get("/")
 async def root():
+    """
+    Root Endpoint
+
+    A simple message to indicate the server is running.
+
+    Returns:
+        dict: A message indicating the server is running.
+
+    """
     return {"message": "Hello World"}
 
 
 @app.get("/secret")
 async def read_item(current_user: User = Depends(auth_service.get_current_user)):
+    """
+    Secret Endpoint
+
+    Access a secret route for authenticated users.
+
+    Args:
+        current_user (User): The currently authenticated user.
+
+    Returns:
+        dict: A secret message along with the owner's email.
+
+    """
     return {"message": 'secret router', "owner": current_user.email}
 
 
 @app.post("/contacts/", response_model=ContactSchema)
 def create_contact(contact: ContactCreate, db: Session = Depends(get_db)):
+    """
+    Create Contact Endpoint
+
+    Create a new contact.
+
+    Args:
+        contact (ContactCreate): Contact information.
+
+    Returns:
+        ContactSchema: The created contact.
+
+    """
     db_contact = Contact(**contact.dict())
     db.add(db_contact)
     db.commit()
@@ -82,6 +172,20 @@ def create_contact(contact: ContactCreate, db: Session = Depends(get_db)):
 
 @app.get("/contacts/", response_model=list[ContactSchema])
 def get_contacts(search_name: str = Query(None), search_email: str = Query(None), db: Session = Depends(get_db)):
+    """
+    Get Contacts Endpoint
+
+    Retrieve a list of contacts.
+
+    Args:
+        search_name (str, optional): Filter contacts by name. Defaults to None.
+        search_email (str, optional): Filter contacts by email. Defaults to None.
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+
+    Returns:
+        list[ContactSchema]: List of contacts.
+
+    """
     query = db.query(Contact)
     if search_name:
         query = query.filter(Contact.first_name.contains(search_name) | Contact.last_name.contains(search_name))
@@ -92,6 +196,22 @@ def get_contacts(search_name: str = Query(None), search_email: str = Query(None)
 
 @app.get("/contacts/{contact_id}", response_model=ContactSchema)
 def get_contact(contact_id: int, db: Session = Depends(get_db)):
+    """
+    Get Contact by ID Endpoint
+
+    Retrieve a contact by its ID.
+
+    Args:
+        contact_id (int): The ID of the contact.
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+
+    Returns:
+        ContactSchema: The requested contact.
+
+    Raises:
+        HTTPException: If the contact is not found.
+
+    """
     contact = db.query(Contact).filter(Contact.id == contact_id).first()
     if contact is None:
         raise HTTPException(status_code=404, detail="Contact not found")
@@ -100,6 +220,23 @@ def get_contact(contact_id: int, db: Session = Depends(get_db)):
 
 @app.put("/contacts/{contact_id}", response_model=ContactSchema)
 def update_contact(contact_id: int, contact: ContactCreate, db: Session = Depends(get_db)):
+    """
+    Update Contact by ID Endpoint
+
+    Update a contact's information.
+
+    Args:
+        contact_id (int): The ID of the contact to be updated.
+        contact (ContactCreate): New contact information.
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+
+    Returns:
+        ContactSchema: The updated contact.
+
+    Raises:
+        HTTPException: If the contact is not found.
+
+    """
     db_contact = db.query(Contact).filter(Contact.id == contact_id).first()
     if db_contact is None:
         raise HTTPException(status_code=404, detail="Contact not found")
@@ -112,6 +249,22 @@ def update_contact(contact_id: int, contact: ContactCreate, db: Session = Depend
 
 @app.delete("/contacts/{contact_id}", response_model=ContactSchema)
 def delete_contact(contact_id: int, db: Session = Depends(get_db)):
+    """
+    Delete Contact by ID Endpoint
+
+    Delete a contact by its ID.
+
+    Args:
+        contact_id (int): The ID of the contact to be deleted.
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+
+    Returns:
+        ContactSchema: The deleted contact.
+
+    Raises:
+        HTTPException: If the contact is not found.
+
+    """
     contact = db.query(Contact).filter(Contact.id == contact_id).first()
     if contact is None:
         raise HTTPException(status_code=404, detail="Contact not found")
@@ -122,6 +275,18 @@ def delete_contact(contact_id: int, db: Session = Depends(get_db)):
 
 @app.get("/upcoming_birthdays", response_model=list[ContactSchema])
 def get_upcoming_birthdays(db: Session = Depends(get_db)):
+    """
+    Get Upcoming Birthdays Endpoint
+
+    Retrieve a list of contacts with upcoming birthdays.
+
+    Args:
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+
+    Returns:
+        list[ContactSchema]: List of contacts with upcoming birthdays.
+
+    """
     today = date.today()
     end_date = today + timedelta(days=7)
     return db.query(Contact).filter(
